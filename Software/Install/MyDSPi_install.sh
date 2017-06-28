52d798fa5b9645585aa59152c94c235e7f079c38
V2Ale <vdelhalle@gmail.com>
2017-06-28 20:57:00 +0200

    update install script

--- Software/Install/MyDSPi_install.sh
+++ Software/Install/MyDSPi_install.sh
@@ -3,6 +3,7 @@
 #Copy the binary file into /usr/bin/
 
 install -D -g root -o root $PWD/MyDSPi /usr/local/bin/MyDSPi
+install -D -g root -o root $PWD/asrc_configurator /usr/local/bin/asrc_configurator
 install -D -g root -o root $PWD/PE_Configurator /usr/local/bin/PE_Configurator
 
 #cp $PWD/MyDSPi_start.sh /usr/local/bin/MyDSPi_start.sh
@@ -12,9 +13,11 @@ install -D -g root -o root $PWD/PE_Configurator /usr/local/bin/PE_Configurator
 install -D -g root -o root $PWD/service/MyDSPi_boot   /etc/init.d;
 chmod 755 /etc/init.d/MyDSPi_boot;
 
-update-rc.d MyDSPi_boot defaults;
+#update-rc.d MyDSPi_boot defaults;
+sudo update-rc.d MyDSPi_boot start 01 2 3 4 5 . stop 01 0 1 6 .
 /etc/init.d/MyDSPi_boot start;
 
 #Copy the default file
 
 install -D -g root -o root $PWD/config/MyDSPi.conf /etc/default/MyDSPi.config
+install -D -g root -o root $PWD/config/asrc.config.default /etc/default/asrc.config.default
